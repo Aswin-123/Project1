cddf4e0871e41cb2f981d793a4a78ed517dd745b
Aswin <59193768+Aswin-123@users.noreply.github.com>
2022-07-13 15:56:04 +0100

    app module modification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule,ReactiveFormsModule} from "@angular/forms";
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './Home.component';
 import { TaskMasterComponent } from './task-master/task-master.component';
-import { RouterModule ,Routes} from '@angular/router';
+import { RouterModule , Routes} from '@angular/router';
 import { LoginComponent } from './account/login/login.component';
 import { RegisterComponent } from './account/register/register.component';
 import { NotFoundComponent } from './not-found.component';
@@ -20,58 +20,58 @@ import { DatePipe } from '@angular/common';
 
 const routes: Routes = [
   {
-    path:"",
+    path: '',
     component : LoginComponent,
   },
   {
-    path:"Register",
+    path: 'Register',
     component : RegisterComponent,
   },
   {
-    path:"Home",
+    path: 'Home',
     component : HomeComponent,
     children: [
       {
-        path: "",
-        canActivateChild:[AuthGuard],
-        data:{routeAccess:'canAccessDashboard'},
+        path: '',
+        canActivateChild: [AuthGuard],
+        data: {routeAccess: 'canAccessDashboard'},
         loadChildren: () => 
-        import("./dash-board/dash-board.module").then(
+        import('./dash-board/dash-board.module').then(
           (u) => u.DashBoardModule
-          ),        
+          ),
       },
       {
-        path: "todo",
-        canActivateChild:[AuthGuard],
-        data:{routeAccess:'canAccessTODO'},
+        path: 'todo',
+        canActivateChild: [AuthGuard],
+        data: {routeAccess: 'canAccessTODO'},
         loadChildren: () => 
-        import("./todo/todo.module").then(
+        import('./todo/todo.module').then(
           (u) => u.TodoModule
-          ),        
+          ),
       },
       {
-        path: "Employee",
-        canActivateChild:[AuthGuard],
-        data:{routeAccess:'canAccessTODO'},
+        path: 'Employee',
+        canActivateChild: [AuthGuard],
+        data: {routeAccess: 'canAccessTODO'},
         loadChildren: () => 
-        import("./employee-details/employee-details.module").then(
+        import('./employee-details/employee-details.module').then(
           (u) => u.EmployeeDetailsModule
-          ),        
+          ),
       },
 
       {
-        path: "task-entry",
-        canActivateChild:[AuthGuard],
-        data:{routeAccess:'canAccessTODO'},
+        path: 'task-entry',
+        canActivateChild: [AuthGuard],
+        data: {routeAccess: 'canAccessTODO'},
         loadChildren: () => 
-        import("./task-entry/task-entry.module").then(
+        import('./task-entry/task-entry.module').then(
           (u) => u.TaskEntryModule
           ),        
       },
     ]   
   },
   {
-    path:"**",
+    path: '**',
     component : NotFoundComponent,
   },
 ];
@@ -94,9 +94,9 @@ const routes: Routes = [
     BrowserAnimationsModule
   ],
   providers: [{
-    provide:HTTP_INTERCEPTORS,
-    useClass:HttpAuthInterceptor,
-    multi:true
+    provide: HTTP_INTERCEPTORS,
+    useClass: HttpAuthInterceptor,
+    multi: true
   },
   DatePipe,
 ],
